4534bb9071edff5f444536b6e30cb4b40aaca4b1
avisekh3110 <avisekh.sabi31102003@gmail.com>
2025-11-09 18:33:04 +0530

    wildcard cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://yapbox.vercel.app"],
+    origin: ["*"],
     credentials: true,
   })
 );
@@ -28,7 +28,7 @@ app.use(
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://yapbox.vercel.app"],
+    origin: ["*"],
     methods: ["GET", "POST"],
     credentials: true,
   },
